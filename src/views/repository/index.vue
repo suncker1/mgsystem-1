8c7961c9180b3512235536978db0c4d3d07e0842
suncker1 <18933643497@163.com>
2020-12-24 16:10:16 +0800

    12-24

--- src/views/repository/index.vue
+++ src/views/repository/index.vue
@@ -1,77 +1,59 @@
 <template>
   <div class="app-container">
-    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
-
-    <el-tree
-      ref="tree2"
-      :data="data2"
-      :props="defaultProps"
-      :filter-node-method="filterNode"
-      class="filter-tree"
-    />
-
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" />
   </div>
 </template>
 
 <script>
 export default {
-
   data() {
-    return {
-      filterText: '',
-      data2: [{
-        id: 1,
-        label: 'Level one 1',
-        children: [{
-          id: 4,
-          label: 'Level two 1-1',
-          children: [{
-            id: 9,
-            label: '11.txt'
-          }, {
-            id: 10,
-            label: '22.jpgfdgfd'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: 'Level one 2',
-        children: [{
-          id: 5,
-          label: '33.mp3'
-        }, {
-          id: 6,
-          label: 'Level two 2-2'
-        }]
-      }, {
-        id: 3,
-        label: 'Level one 3',
-        children: [{
-          id: 7,
-          label: 'Level two 3-1'
-        }, {
-          id: 8,
-          label: 'Level two 3-2'
-        }]
-      }],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      }
-    }
+    return {}
   },
-  watch: {
-    filterText(val) {
-      this.$refs.tree2.filter(val)
-    }
-  },
-
+  // watch: {
+  //   filterText(val) {
+  //     this.$refs.tree2.filter(val)
+  //   }
+  // },
   methods: {
-    filterNode(value, data) {
-      if (!value) return true
-      return data.label.indexOf(value) !== -1
+    handleNodeClick(data) {
+      console.log(data)
+    },
+    handleCheckChange(data, checked, indeterminate) {
+      console.log(data, checked, indeterminate)
+    },
+    loadNode(node, resolve) {
+      if (node.level === 0) {
+        return resolve([{ name: 'region1' }, { name: 'region2' }])
+      }
+      if (node.level > 3) return resolve([])
+
+      var hasChild
+      if (node.data.name === 'region1') {
+        hasChild = true
+      } else if (node.data.name === 'region2') {
+        hasChild = false
+      } else {
+        hasChild = Math.random() > 0.5
+      }
+
+      setTimeout(() => {
+        var data
+        if (hasChild) {
+          data = [
+            {
+              name: 'zone' + this.count++
+            },
+            {
+              name: 'zone' + this.count++
+            }
+          ]
+        } else {
+          data = []
+        }
+
+        resolve(data)
+      }, 500)
     }
   }
 }
 </script>
-
